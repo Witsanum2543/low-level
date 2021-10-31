0da2ff5f1ec6e139b4557258155a63376b3d7cae
witsanu <witsanum2543@hotmail.com>
2021-10-31 19:12:51 +0700

    b2

--- test.c
+++ test.c
@@ -6,8 +6,9 @@ int main(int argc, char* argv[]) {
 
     char buf[1000];
     printf("Write input text: ");
-    fgets(buf, 1000, stdin);
-    printf("%s\n", buf);
+    while (fgets(buf, 1000, stdin)){
+        printf("%s\n", buf);
+    }
 
     exit(EXIT_SUCCESS);
 }
